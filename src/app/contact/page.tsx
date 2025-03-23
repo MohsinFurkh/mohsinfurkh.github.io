ab176c7bea8b8f12ebdedde2f8f8ae3cfe41e84d
Mohsin Furkh <55917187+MohsinFurkh@users.noreply.github.com>
2025-03-23 21:59:04 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -72,8 +72,8 @@ export default function Contact() {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
-                    <a href="mailto:mohsin.dar@uohyd.ac.in" className="hover:underline">
-                      mohsin.dar@uohyd.ac.in
+                    <a href="mailto:20mcpc02@uohyd.ac.in" className="hover:underline">
+                      20mcpc02@uohyd.ac.in
                     </a>
                   </p>
                 </div>
@@ -85,7 +85,7 @@ export default function Contact() {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                    Department of Computer Science<br />
+                    School of Computer and Information Sciences<br />
                     University of Hyderabad<br />
                     Hyderabad, India
                   </p>
